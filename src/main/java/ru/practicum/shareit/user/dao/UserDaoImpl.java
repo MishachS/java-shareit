aafb0986b108007e4f056c51a5e3f273bd63dd99
MishachS <sashamih12@yandex.ru>
2024-01-26 20:03:12 +0300

    Исправил ошибки pull-request 3

--- src/main/java/ru/practicum/shareit/user/dao/UserDaoImpl.java
+++ src/main/java/ru/practicum/shareit/user/dao/UserDaoImpl.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class UserDaoImpl implements UserDao {
     private int nextId = 1;
     HashMap<Integer, User> userStorage = new HashMap<>();
+
     @Override
     public User addUser(User user) {
         checkUserEmail(user);
